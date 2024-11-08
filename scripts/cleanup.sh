3b5ec8364253341bba3a147e1b2f5c5ab53ff409
Aleksandr Duplenko <reversenant@mail.ru>
2024-11-08 10:15:19 +0200

    1

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -6,6 +6,7 @@ find "$repo_dir" -type f -name "metainfo.yaml" | while read -r file; do
   if ! grep -q 'ObjectId: PT' "$file"; then
     echo "Deleting folder: $(dirname "$file")"
     rm -rf "$(dirname "$file")"
+  fi
   if ! grep -q 'ObjectId: SEC-RF-126634467' "$file"; then
     echo "Deleting folder: $(dirname "$file")"
     rm -rf "$(dirname "$file")"
